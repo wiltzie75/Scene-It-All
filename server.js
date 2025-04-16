752e91d106e0c7f3124fb80e92f2cc819eb0fdc8
Olivia <yutongchen27@hotmail.com>
2025-04-16 11:49:40 -0700

    updated  auth.js and updated auth and comments routes in the server.

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const movieRoutes = require('./routes/movie');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 
-// 使用中间件解析 JSON 请求体
+const authRoutes = require('./routes/auth');
+const commentsRoutes = require('./routes/comments');
+
+
 app.use(express.json());
 
-// 设置路由
+// set api
 app.use('/api/movies', movieRoutes);
 app.use('/api/reviews', reviewRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/auth',authRoutes);
+app.use('/api/comments',commentsRoutes);
+ 
 
-// 设置服务器端口并启
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
