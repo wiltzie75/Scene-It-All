f4d464f91cabaaeb1834bfd3490dad5d2b5b3ef5
Eric Graham <wiltzie.eg@gmail.com>
2025-04-18 17:12:58 -0400

    fixed profile page

--- cap/src/components/Profile.jsx
+++ cap/src/components/Profile.jsx
@@ -2,11 +2,17 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../api/api";
 import Movies from "./Movies";
+import { border } from "@mui/system";
 
 const Profile = () => {
     const navigate = useNavigate();
-    const [profile, setProfile] = useState({});
-    const [favorites, setFavorites] = useState([]);
+    const [profile, setProfile] = useState({
+        favorites: [],
+        reviews: [],
+        comments: [],
+        firstName: "",
+        lastName: ""
+    });
 
     const fetchProfile = async (token) => {
         try {
@@ -26,7 +32,7 @@ const Profile = () => {
         async function getProfile() {
             const token = localStorage.getItem("token");
             if (!token) {
-                navigate("/login"); // optional
+                navigate("/login");
                 return;
             }
     
@@ -38,22 +44,9 @@ const Profile = () => {
         getProfile();
     }, []);
 
-    useEffect(() => {
-        const fetchFavorites = async () => {
-            try {
-                const response = await fetch(`${API}/top-rated`);
-                const data = await response.json();
-                setFavorites(data);
-            } catch (error) {
-                console.log("Error fetching your favorite movies:", error);
-            }
-        };
-        fetchFavorites();
-    }, []);
-
     return ( 
         <>
-            <div>
+            <div style={{border: '1px solid black'}}>
                 {profile && (
                     <div>
                         <h2 style={{ textAlign: "center" }}>
@@ -63,20 +56,74 @@ const Profile = () => {
                 )}
             </div>
 
-            <div>
-                {favorites.length === 0 ? (
+            <div style={{border: '1px solid black'}}>
+                {profile && profile.favorites.length === 0 ? (
                     <div>
                         <h3>You have no Favorites</h3>
                     </div>
                 ) : (
                     <div>
                         <h3>My Favorite Movies</h3>
-                        {favorites.map((movie, index) => (
+                        {profile.favorites?.map((movie) => (
                             <div key={movie.id}>
-                                <img src={movie.poster} alt={movie.title} />
-                                <h4>{movie.title}</h4>
+                                <img src={movie.movie?.poster} alt={movie.movie?.title} />
+                                <h4>{movie.movie?.title}</h4>
                             </div>
                         ))}
+                    </div>
+                )}
+            </div>
+
+            <div style={{border: '1px solid black'}}>
+                {profile && profile.reviews.length === 0 ? (
+                    <div>
+                        <h3>You have no reviews</h3>
+                    </div>
+                ) : (
+                    <div>
+                        <h3>My Reviews</h3>
+                        {profile.reviews.map((review) => (
+                            <div key={review.id}>
+                                <h4>{review.movie?.title}</h4>
+                                <p>{review.subject}: {review.description}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+
+
+            <div style={{border: '1px solid black'}}>
+                {profile && profile.comments.length === 0 ? (
+                    <div>
+                        <h3>You have no comments</h3>
+                    </div>
+                ) : (
+                    <div>
+                        <h3>My Comments</h3>
+                        {profile.comments.map((comment) => (
+                            <div key={comment.id}>
+                                <h4>{comment.review?.movie?.title}</h4>
+                                <p>{comment.subject}: {comment.description}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+
+            <div style={{border: '1px solid black'}}>
+                {profile && profile.isAdmin === true && (
+                    <div>
+                        <h2>Admin Panel</h2>
+                        <div>
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    
                     </div>
                 )}
             </div>
