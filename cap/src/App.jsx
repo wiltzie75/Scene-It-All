d2cb76fb3dd7dd1044688e9deddff42315fcf079
Eric Graham <wiltzie.eg@gmail.com>
2025-04-21 11:27:31 -0400

    created recent reviews.jsx

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./components/Login";
 import Profile from "./components/Profile";
 // import TopRated from "/TopRated";
 import Movies from "./components/Movies";
+import RecentReviews from "./components/RecentReviews";
 
   function App() {
     const [count, setCount] = useState(0);
@@ -31,6 +32,7 @@ import Movies from "./components/Movies";
             <Route path="/users/register" element={<Register token={token} setToken={setToken}/>} />
             <Route path="/users/login" element={<Login token={token} setToken={setToken} />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/recentreviews" element={<RecentReviews />} />
             {/* <Route path="/top rated" element={<TopRated />} /> */}
             {/* <Route
             path="/admin"
