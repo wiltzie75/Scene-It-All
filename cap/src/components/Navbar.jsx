0bdef81eab09323358080ea410a64f68b25f9bbc
Daniel Garibay <danielgaribay366@gmail.com>
2025-04-22 12:42:34 -0500

    added search bar and fixed a couple fitment things

--- cap/src/components/Navbar.jsx
+++ cap/src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import {
   Toolbar,
   Button,
   Typography,
-  IconButton,
   Box,
 } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
@@ -28,35 +27,39 @@ export default function Navbar() {
     <>
       <AppBar position="fixed" sx={{ zIndex: 10 }}>
         <Toolbar>
-          <Typography variant="h5" sx={{ flex: 1}}>
+          <Typography variant="h5" sx={{ flex: 1 }}>
             Scene It All
           </Typography>
 
+          {/* Navigation buttons */}
           <Button color="inherit" component={Link} to="/">
             Home
           </Button>
-
           <Button color="inherit" component={Link} to="/Movies">
             Movies
           </Button>
-
           <Button color="inherit" component={Link} to="/">
             Top Rated
           </Button>
 
-        <Box sx={{ ml: 2 }}>
-          {isLoggedIn ? (
-            <Button color="inherit" onClick={handleLogout}>
-              Logout
-            </Button>
-          ) : (
-            <IconButton color="inherit">
-              {/* You can add an icon here if needed */}
-            </IconButton>
-          )}
-        </Box>
-      </Toolbar>
-    </AppBar>
+          <Box sx={{ ml: 2 }}>
+            {isLoggedIn ? (
+              <Button color="inherit" onClick={handleLogout}>
+                Logout
+              </Button>
+            ) : (
+              <>
+                <Button color="inherit" component={Link} to="/login">
+                  Login
+                </Button>
+                <Button color="inherit" component={Link} to="/register">
+                  Register
+                </Button>
+              </>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
     </>
   );
 }
