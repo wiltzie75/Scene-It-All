46fec7c49dc9e4d549d4d9a82883939ee36f14d6
Eric Graham <wiltzie.eg@gmail.com>
2025-04-24 18:07:00 -0400

    styled movies.jsx

--- cap/src/components/Movies.jsx
+++ cap/src/components/Movies.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { TextField, Box, Typography, Card, CardMedia, CardContent, Dialog, DialogContent, DialogTitle, Button } from "@mui/material";
+import { TextField, Box, Typography, Card, CardMedia, CardContent, Dialog, DialogContent, DialogTitle, Button, Pagination } from "@mui/material";
 import "../App.css";
 import API from "../api/api";
 // import { hasCustomParams } from "react-admin";
@@ -15,6 +15,8 @@ const Movies = () => {
   const [isCommentDialogOpen, setIsCommentDialogOpen] = useState(false);
   const [activeReviewId, setActiveReviewId] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
   useEffect(() => {
 
@@ -176,118 +178,148 @@ const Movies = () => {
     movie?.title?.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const indexOfLastMovie = currentPage * itemsPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - itemsPerPage;
+  const currentMovies = filteredMovies.slice(indexOfFirstMovie, indexOfLastMovie);
+
+  const totalPages = Math.ceil(filteredMovies.length / itemsPerPage);
+
   return (
-    <Box sx={{ maxWidth: 900, margin: "auto", p: 3 }}>
+    <Box sx={{ maxWidth: 1200, margin: "0 auto", gap: 2, mt: 6,p: 2, display: "flex", flexDirection: "column" }}>
       {/* Search Bar */}
-      <Box sx={{ mb: 3, display: "flex", justifyContent: "center" }}>
+      <Box sx={{ mb: 1, display: "flex", justifyContent: "center" }}>
         <TextField
           variant="outlined"
           label="Search for a movie..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          size="small"
           sx={{
-            width: { xs: "90%", sm: "70%", md: "60%" },
+            width: "60%",
             bgcolor: "#f9f9f9",
+            fontSize: "0.8rem",
             borderRadius: 1,
             "& .MuiOutlinedInput-root": {
               "& fieldset": { borderColor: "gray" },
               "&:hover fieldset": { borderColor: "#333" },
               "&.Mui-focused fieldset": { borderColor: "#1976d2" },
+              height: "36px"
             },
           }}
         />
       </Box>
 
       {/* Movie List */}
-      <Box sx={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", gap: 2 }}>
-        {filteredMovies.length > 0 ? (
-          filteredMovies.map((movie) => (
+      <Box sx={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", gap: 3, fontSize: "0.2rem" }}>
+        {currentMovies.length > 0 ? (
+          currentMovies.map((movie) => (
             <Card key={movie.id} onClick={() => setSelectedMovie(movie)} sx={{ cursor: "pointer", bgcolor: "#fff", transition: "0.3s", "&:hover": { bgcolor: "#f0f0f0" } }}>
-              <CardMedia component="img" height="300" image={movie.poster} alt={movie.title} />
-              <CardContent>
-                <Typography variant="h6">{movie.title}</Typography>
-              </CardContent>
+              <CardMedia component="img" height="325" image={movie.poster} alt={movie.title} />
+              {/* <CardContent>
+                <Typography variant="h6" sx={{fontSize: ".75rem"}}  >{movie.title}</Typography>
+              </CardContent> */}
             </Card>
           ))
         ) : (
           <Typography>No movies found.</Typography>
         )}
       </Box>
+      {totalPages > 1 && (
+        <Box sx={{ mt: 1, display: "flex", justifyContent: "center" }}>
+          <Pagination
+            count={totalPages}
+            page={currentPage}
+            onChange={(e, value) => setCurrentPage(value)}
+            color="primary"
+            shape="rounded"
+            size="small"
+          />
+        </Box>
+      )}
 
       {/* Movie Details Dialog */}
-      <Dialog open={!!selectedMovie} onClose={() => setSelectedMovie(null)} maxWidth="sm" fullWidth>
+      <Dialog open={!!selectedMovie} onClose={() => setSelectedMovie(null)} maxWidth="md" fullWidth>
         {selectedMovie && (
           <>
-            <DialogTitle>{selectedMovie.title}</DialogTitle>
             <DialogContent>
+            <Box sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" }, gap: 3 }}>
+            <Card key={selectedMovie.id} sx={{ cursor: "pointer", maxHeight: 220, maxWidth: 160 }}>
             <CardMedia
-            component="img"
-            height="400"
-            image={selectedMovie.poster}
-            alt={selectedMovie.title}
-            sx={{ objectFit: "contain", width: "100%" }}/>
-              <Typography variant="body1" sx={{ mt: 2 }}><strong>Description:</strong> {selectedMovie.plot}</Typography>
-              <Typography variant="body1"><strong>Rating:</strong> {selectedMovie.imdbRating}</Typography>
-              <Typography variant="body1"><strong>My Rating:</strong> {selectedMovie.userRatings}</Typography>
-              <Typography variant="body1"><strong>Year:</strong> {selectedMovie.year}</Typography>
-              <Typography variant="body1"><strong>Genre:</strong> {selectedMovie.genre}</Typography>
-              <br></br>
-              <Typography variant="body1"><strong>Reviews:</strong></Typography>
-              {selectedMovie.reviews && selectedMovie.reviews.length > 0 ? (
-                selectedMovie.reviews.map((review) => (
-                  <Box key={review.id} sx={{ mt: 2 }}>
-                    <Typography variant="subtitle1">{`
-                      ${review.user.firstName} ${review.user.lastName} wrote:`}</Typography>
-                    <Typography variant="subtitle1">{review.subject}</Typography>
-                    <Typography variant="body2">{review.description}</Typography>
-                    <Box sx={{ mt: 4 }}>
-                    {renderComments(review)}
-                    {isLoggedIn && (
-                      <>
-                        <Button onClick={() => {setActiveReviewId(review.id); setIsCommentDialogOpen(true)}}>Add a Comment</Button>
-                      </>
-                    )}
-                    <Dialog open={isCommentDialogOpen} onClose={() => setIsCommentDialogOpen(false)}>
+              component="img"
+              image={selectedMovie.poster}
+              alt={selectedMovie.title}
+              sx={{ height: "auto", width: "100%" }}
+            />
+              <CardContent sx={{ p: 1 }}>
+                <Typography variant="body2" noWrap>{selectedMovie.title}</Typography>
+              </CardContent>
+            </Card>
 
-                      <DialogTitle>Add a Comment</DialogTitle>
-                      <DialogContent>
-                        <TextField
-                          label="Subject"
-                          fullWidth
-                          value={addComment.subject}
-                          onChange={(e) =>
-                            setAddComment({ ...addComment, subject: e.target.value })
-                          }
-                          sx={{ mb: 2 }}
-                        />
-                        <TextField
-                          label="Description"
-                          fullWidth
-                          multiline
-                          rows={4}
-                          value={addComment.description}
-                          onChange={(e) =>
-                            setAddComment({ ...addComment, description: e.target.value })
-                          }
-                        />
-                        <Box sx={{ mt: 2, display: "flex", justifyContent: "flex-end", gap: 1 }}>
-                          <Button onClick={() => setIsCommentDialogOpen(false)} color="error">
-                            Cancel
-                          </Button>
-                          <Button
-                            variant="contained"
-                            onClick={() => {
-                              handleCommentSubmit(activeReviewId);
-                            }}
-                          >
-                            Save
-                          </Button>
-                        </Box>
-                      </DialogContent>
-                    </Dialog>
-                  </Box>
-                  </Box>
-                ))
+            <Box sx={{ flex: 1}}>
+              <Typography variant="body1" sx={{ mb: 1 }}><strong>Description:</strong> {selectedMovie.plot}</Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}><strong>Rating:</strong> {selectedMovie.imdbRating}</Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}><strong>My Rating:</strong> {selectedMovie.userRatings}</Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}><strong>Year:</strong> {selectedMovie.year}</Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}><strong>Genre:</strong> {selectedMovie.genre}</Typography>
+            </Box>
+            </Box>
+            <br></br>
+            <Typography variant="body1"><strong>Reviews:</strong></Typography>
+            {selectedMovie.reviews && selectedMovie.reviews.length > 0 ? (
+              selectedMovie.reviews.map((review) => (
+                <Box key={review.id} sx={{ mb: 1 }}>
+                  <Typography variant="subtitle1">{`
+                    ${review.user.firstName} ${review.user.lastName} wrote:`}</Typography>
+                  <Typography variant="subtitle1">{review.subject}</Typography>
+                  <Typography variant="body2">{review.description}</Typography>
+                  <Box sx={{ mt: 4 }}>
+                  {renderComments(review)}
+                  {isLoggedIn && (
+                    <>
+                      <Button onClick={() => {setActiveReviewId(review.id); setIsCommentDialogOpen(true)}}>Add a Comment</Button>
+                    </>
+                  )}
+                  <Dialog open={isCommentDialogOpen} onClose={() => setIsCommentDialogOpen(false)}>
+
+                    <DialogTitle>Add a Comment</DialogTitle>
+                    <DialogContent>
+                      <TextField
+                        label="Subject"
+                        fullWidth
+                        value={addComment.subject}
+                        onChange={(e) =>
+                          setAddComment({ ...addComment, subject: e.target.value })
+                        }
+                        sx={{ mb: 2 }}
+                      />
+                      <TextField
+                        label="Description"
+                        fullWidth
+                        multiline
+                        rows={4}
+                        value={addComment.description}
+                        onChange={(e) =>
+                          setAddComment({ ...addComment, description: e.target.value })
+                        }
+                      />
+                      <Box sx={{ mt: 2, display: "flex", justifyContent: "flex-end", gap: 1 }}>
+                        <Button onClick={() => setIsCommentDialogOpen(false)} color="error">
+                          Cancel
+                        </Button>
+                        <Button
+                          variant="contained"
+                          onClick={() => {
+                            handleCommentSubmit(activeReviewId);
+                          }}
+                        >
+                          Save
+                        </Button>
+                      </Box>
+                    </DialogContent>
+                  </Dialog>
+                </Box>
+                </Box>
+              ))
               ) : (
                 <Typography>No reviews yet.</Typography>
               )}
