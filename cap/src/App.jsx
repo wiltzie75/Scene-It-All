b65cca28d583844f31cd5991460933101d343a7e
Ben Hogle <ben.hogle32@gmail.com>
2025-04-25 12:06:46 -0400

    second

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -12,13 +12,13 @@ import Movies from "./components/Movies";
 import MyReviews from "./components/MyReviews";
 import MyComments from "./components/MyComments";
 import Users from "./components/Users";
-import AdminMovies from './components/AdminMovies';
+import AdminMovies from "./components/AdminMovies";
 import Reviews from "./components/Reviews";
 
 function App() {
   const [count, setCount] = useState(0);
   const [token, setToken] = useState(null);
-  
+
   let user = null;
   try {
     const storedUser = localStorage.getItem("user");
@@ -36,7 +36,7 @@ function App() {
         minHeight: "100vh",
       }}
     >
-      <Navbar token={token} setToken={setToken}/>
+      <Navbar token={token} setToken={setToken} />
 
       <Container sx={{ flex: 1, mt: 4 }}>
         {/* Only show "Go to Admin Page" button if user is admin */}
@@ -47,21 +47,30 @@ function App() {
             </Link>
           </div>
         )}
-        
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/reviews" element={<Reviews />} />
-            <Route path="/register" element={<Register token={token} setToken={setToken}/>} />
-            <Route path="/top-rated" element={<TopRated />} />
-            <Route path="/login" element={<Login token={token} setToken={setToken} />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/myreviews" element={<MyReviews />} />
-            <Route path="/mycomments" element={<MyComments />} />
-            <Route path="/users" element={<Users />} />
-            
-            {/*  Only show the admin page if the user is an admin  */}
-            <Route path="/admin" element={user?.isAdmin ? <AdminMovies /> : <Navigate to="/" />} />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route
+            path="/register"
+            element={<Register token={token} setToken={setToken} />}
+          />
+          <Route path="/topRated" element={<TopRated />} />
+          <Route
+            path="/login"
+            element={<Login token={token} setToken={setToken} />}
+          />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/myreviews" element={<MyReviews />} />
+          <Route path="/mycomments" element={<MyComments />} />
+          <Route path="/users" element={<Users />} />
+
+          {/*  Only show the admin page if the user is an admin  */}
+          <Route
+            path="/admin"
+            element={user?.isAdmin ? <AdminMovies /> : <Navigate to="/" />}
+          />
         </Routes>
       </Container>
 
