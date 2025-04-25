37536c1a079d6e4ae0cc7ee3dee354ff5dde62af
Eric Graham <wiltzie.eg@gmail.com>
2025-04-25 15:17:07 -0400

    fixed watchlist, made sure everything is now Favorite

--- routes/favorite.js
+++ routes/favorite.js
@@ -4,23 +4,23 @@ const verifyToken = require('./verify');
 const prisma = require('../prisma');
 
 
-// GET user watchlist
-router.get('/:id/watchlist', async(req, res) => {
+// GET user favorite
+router.get('/:id/favorite', async(req, res) => {
     const { id } = req.params;
     try{
-        const watchlist = await prisma.favorite.findMany({
+        const favorite = await prisma.favorite.findMany({
             where: { userId: Number(id) },
             include: { movie: true },
         });
     
-        res.json(watchlist.map(fav => fav.movie));
+        res.json(favorite.map(fav => fav.movie));
     } catch (error) {
         console.log(error);
         res.status(500).json({message:'Server error'})
     }
 })
 
-// add item to watchlist
+// add item to favorite
 router.post('/',verifyToken, async(req, res) => {
     const { userId, movieId } = req.body;
     console.log(req.user);
@@ -39,8 +39,8 @@ router.post('/',verifyToken, async(req, res) => {
 
     const addFavorite = await prisma.favorite.create({
         data: {
-            userId,
-            movieId,
+            userId: Number(userId),
+            movieId: Number(movieId),
         },
     });
 
@@ -51,7 +51,7 @@ router.post('/',verifyToken, async(req, res) => {
     }
 });
 
-// delete item from watchlist
+// delete item from favorite
 router.delete('/:userId/:movieId', async(req, res) => {
     const { userId, movieId } = req.params;
     try{
@@ -63,7 +63,7 @@ router.delete('/:userId/:movieId', async(req, res) => {
                 }
             }
         });
-        res.json({message: `Movie ${movieId} removed from watchlist.`});
+        res.json({message: `Movie ${movieId} removed from favorite.`});
     } catch (error) {
         console.log(error);
         res.status(500).json({message:'Server error'})
