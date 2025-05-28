b132928e90b302ec018e7bac4ba1082d425a5880
Eric Graham <8397596+wiltzie75@users.noreply.github.com>
2025-05-28 18:46:12 -0400

    testing server.js 19

--- cap/src/api/api.js
+++ cap/src/api/api.js
@@ -1,5 +1,11 @@
-// import axios from "axios";
+// // import axios from "axios";
 
-const API = "http://localhost:3000/api";
+// const API = "http://localhost:3000/api";
+
+// export default API;
+
+const API = process.env.NODE_ENV === 'production' 
+  ? '/api'  // Use relative path in production
+  : 'http://localhost:3000/api';  // Use localhost in development
 
 export default API;
