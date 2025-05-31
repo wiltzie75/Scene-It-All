34824f79f957468dd1eef164b7e5bc94d0e82c2b
Eric Graham <8397596+wiltzie75@users.noreply.github.com>
2025-05-31 14:45:14 -0400

    testing server.js 29

--- cap/src/api/api.js
+++ cap/src/api/api.js
@@ -1,7 +1,12 @@
-const API = process.env.NODE_ENV === 'production'    
-  ? '/api'  // Use relative path in production   
-  : 'http://localhost:3000/api';  // Use localhost in development  
 
-console.log('API URL:', API, 'NODE_ENV:', process.env.NODE_ENV); // Add for debugging
-
+const API = '/api';  // Always use relative path since everything runs on same service
+console.log('API URL:', API, 'NODE_ENV:', process.env.NODE_ENV);
 export default API;
+
+// const API = process.env.NODE_ENV === 'production'    
+//   ? '/api'  // Use relative path in production   
+//   : 'http://localhost:3000/api';  // Use localhost in development  
+
+// console.log('API URL:', API, 'NODE_ENV:', process.env.NODE_ENV); // Add for debugging
+
+// export default API;
